1c9cffff892971627c1395897061c9b7773301f6
Julio Legrec <julio.legrec@gmail.com>
2023-09-29 08:38:50 +0400

    refined button + added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,27 @@ import Button from './components/Button';
 function App() {
 	return (
 		<>
-			<Button />
+			<Button
+				config={{
+					type: 'rock',
+					color: 'hsl(349, 70%, 56%)',
+					shadow: 'hsl(349, 70%, 36%)',
+				}}
+			/>
+			<Button
+				config={{
+					type: 'paper',
+					color: 'hsl(230, 89%, 65%)',
+					shadow: 'hsl(230, 89%, 45%)',
+				}}
+			/>
+			<Button
+				config={{
+					type: 'scissors',
+					color: 'hsl(39, 89%, 49%)',
+					shadow: 'hsl(39, 89%, 29%)',
+				}}
+			/>
 		</>
 	);
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Button() {
+const GuessButton = styled.button`
+	${(props) => {
+		return `
+			background-color: white;
+			border: 20px solid ${props.color};
+			border-radius: 100%;
+			padding: 20px;
+			aspect-ratio: 1/1;
+			width: 10rem;
+			cursor: pointer;
+			box-shadow: inset 0 8px rgba(0, 0, 0, 0.1), 0 8px 0 ${props.shadow};
+
+			&:active {
+				box-shadow: none;
+				transform: translateY(8px);
+			}
+		`;
+	}}
+`;
+
+export default function Button({ config }) {
+	function handleInput(e) {
+		console.log(e.currentTarget.value);
+	}
+
 	return (
-		<button
-			style={{
-				backgroundColor: 'white',
-				border: '20px solid hsl(349, 71%, 52%)',
-				borderRadius: '100%',
-				padding: '20px',
-				aspectRatio: '1/1',
-				width: '10rem',
-				cursor: 'pointer',
-				boxShadow: 'inset 0 8px rgba(0,0,0,0.1), 0 8px 0 hsl(349, 76%, 35%)',
-			}}
+		<GuessButton
+			color={config.color}
+			shadow={config.shadow}
+			value={config.type}
+			onClick={(e) => handleInput(e)}
 		>
-			<img src="./images/icon-rock.svg" alt="" />
-		</button>
+			<img src={`./images/icon-${config.type}.svg`} alt="guess-icon" />
+		</GuessButton>
 	);
 }
