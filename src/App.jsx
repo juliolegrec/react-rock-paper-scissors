555e691669640f5f47d36907c0defbb0e13f6f18
Julio Legrec <julio.legrec@gmail.com>
2023-11-27 10:03:54 +0400

    added sub container width

--- src/App.jsx
+++ src/App.jsx
@@ -8,66 +8,68 @@ import Rules from './components/Rules';
 const choices = ['rock', 'paper', 'scissors'];
 
 function App() {
-	const [userChoice, setUserChoice] = useState(null);
-	const [computerChoice, setComputerChoice] = useState(null);
-	const [result, setResult] = useState(null);
-	const [score, setScore] = useState(0);
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
+  const [score, setScore] = useState(0);
 
-	const generateComputerChoice = () => {
-		const randomIndex = Math.floor(Math.random() * choices.length);
-		return choices[randomIndex];
-	};
+  const generateComputerChoice = () => {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  };
 
-	const determineWinner = (user, computer) => {
-		if (user === computer) return "It's a tie";
-		if (
-			(user === 'rock' && computer === 'scissors') ||
-			(user === 'paper' && computer === 'rock') ||
-			(user === 'scissors' && computer === 'paper')
-		) {
-			setTimeout(() => {
-				setScore(score + 1);
-			}, 2000);
-			return 'You win';
-		} else {
-			return 'You lose';
-		}
-	};
+  const determineWinner = (user, computer) => {
+    if (user === computer) return "It's a tie";
+    if (
+      (user === 'rock' && computer === 'scissors') ||
+      (user === 'paper' && computer === 'rock') ||
+      (user === 'scissors' && computer === 'paper')
+    ) {
+      setTimeout(() => {
+        setScore(score + 1);
+      }, 2000);
+      return 'You win';
+    } else {
+      return 'You lose';
+    }
+  };
 
-	const handleUserChoice = (choice) => {
-		const computerChoice = generateComputerChoice();
-		setUserChoice(choice);
-		setComputerChoice(computerChoice);
-		const result = determineWinner(choice, computerChoice);
-		setResult(result);
-	};
+  const handleUserChoice = (choice) => {
+    const computerChoice = generateComputerChoice();
+    setUserChoice(choice);
+    setComputerChoice(computerChoice);
+    const result = determineWinner(choice, computerChoice);
+    setResult(result);
+  };
 
-	const reset = () => {
-		setComputerChoice(null);
-	};
+  const reset = () => {
+    setComputerChoice(null);
+  };
 
-	return (
-		<>
-			<div className="container">
-				<TitleScoreBar score={score} />
+  return (
+    <>
+      <div className="container">
+        <div className="sub-container">
+          <TitleScoreBar score={score} />
 
-				{!computerChoice && (
-					<ChoiceArea bonusGame={false} getUserChoice={handleUserChoice} />
-				)}
+          {!computerChoice && (
+            <ChoiceArea bonusGame={false} getUserChoice={handleUserChoice} />
+          )}
+        </div>
 
-				{computerChoice && (
-					<FaceOff
-						userChoice={userChoice}
-						computerChoice={computerChoice}
-						result={result}
-						reset={reset}
-					/>
-				)}
-			</div>
+        {computerChoice && (
+          <FaceOff
+            userChoice={userChoice}
+            computerChoice={computerChoice}
+            result={result}
+            reset={reset}
+          />
+        )}
+      </div>
 
-			<Rules bonusGame={false} />
-		</>
-	);
+      <Rules bonusGame={false} />
+    </>
+  );
 }
 
 export default App;
